2b5e741aeec35d453751f0b5fd4038bc31d34b1d
aldopanjaitan <aldopanjaitan23@gmail.com>
2022-04-06 18:17:03 +0700

    perubahan nav dan tambahan main.js serta perubahan output.css baris 486

--- src/main.js
+++ src/main.js
@@ -1,1 +1,24 @@
-import "tw-elements";
+function dropdowns() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("desknav").style.backgroundColor = "whitesmoke";
+    document.getElementById("desknav").style.padding = "20px 0";
+    document.getElementById("myLinks").style.backgroundColor = "whitesmoke";
+  } else {
+    document.getElementById("desknav").style.backgroundColor = "transparent";
+    document.getElementById("desknav").style.padding = "20px 0";
+    document.getElementById("desknav").style.padding = "auto";
+    document.getElementById("myLinks").style.backgroundColor = "white";
+  }
+}
